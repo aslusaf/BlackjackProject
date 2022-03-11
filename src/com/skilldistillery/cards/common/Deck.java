b40d52574197664f8164d44d52083652326dd3e9
aslusaf <aslusaf@live.com>
2022-03-11 15:19:52 -0700

    Add common card classes

---
+++ src/com/skilldistillery/cards/common/Deck.java
@@ -0,0 +1,36 @@
+package com.skilldistillery.cards.common;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+
+	ArrayList<Card> deck = new ArrayList<>(52);
+
+	public Deck() {
+
+		for (Suit suit : Suit.values()) {
+
+			for (Rank rank : Rank.values()) {
+
+				deck.add(new Card(suit, rank));
+
+			}
+
+		}
+
+	}
+
+	public int checkDeckSize() {
+		return deck.size();
+	}
+
+	public Card dealCard() {
+		return deck.remove(0);
+	}
+
+	public void shuffleDeck() {
+		Collections.shuffle(deck);
+	}
+
+}
